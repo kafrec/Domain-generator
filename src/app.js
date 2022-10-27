6951f065bc800e179b937b2c2751cd3a786f2d84
karen <karemfreya@gmail.com>
2022-10-27 21:18:27 +0000

    cambios2

--- src/app.js
+++ src/app.js
@@ -7,20 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  var nombres = ["freya", "luna", "yinyang", "zayen", "tutu", "stich"];
-  var adjetivos = ["genial", "linda", "lindo", "fuerte", "grande", "regalon"];
-  var otros = [".com", ".cl", ".es", ".org", ".arg", ".col"];
+  let nombres = ["freya", "luna", "yinyang", "zayen", "tutu", "stich"];
+  let adjetivos = ["genial", "linda", "lindo", "fuerte", "grande", "regalon"];
+  let otros = [".com", ".cl", ".es", ".org", ".arg", ".col"];
 
-  let nuevoDominio = [];
+  let nuevoDominio = "";
   for (let i = 0; i < nombres.length; i++) {
     for (let j = 0; j < adjetivos.length; j++) {
       for (let k = 0; k < otros.length; k++) {
-        console.log("www." + nombres[i] + adjetivos[j] + otros[k]);
+        //console.log(nombres[i] + adjetivos[j] + otros[k]);
+        nuevoDominio = nombres[i] + adjetivos[j] + otros[j];
       }
     }
   }
 
-  document.getElementById("respuesta").innerHTML = "como estas";
+  document.getElementById("respuesta").innerHTML = nuevoDominio;
 
   console.log("Hello Rigo from the console!");
 };
